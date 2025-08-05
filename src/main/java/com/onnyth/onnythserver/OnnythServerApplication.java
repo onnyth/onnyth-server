a62f19dcf88af4de4760d8b7c20a8f89db9f10d2
MusharafZM <musharafzm7@gmail.com>
2025-08-05 07:34:30 +0530

    Entry class

---
+++ src/main/java/com/onnyth/onnythserver/OnnythServerApplication.java
@@ -0,0 +1,13 @@
+package com.onnyth.onnythserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnnythServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnnythServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onnyth/onnythserver/OnnythServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onnyth.onnythserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnnythServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
